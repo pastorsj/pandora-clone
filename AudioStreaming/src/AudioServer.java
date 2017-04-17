d1f7bdfc14516f800f43da9c52fd7010411b1ea7
pastorsj <pastorsj@rose-hulman.edu>
2017-04-16 22:27:59 -0400

    Allows multiple connections to server

--- AudioStreaming/src/AudioServer.java
+++ AudioStreaming/src/AudioServer.java
@@ -1,29 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by sampastoriza on 4/16/17.
  */
-import java.io.*;
-import java.net.*;
 
 public class AudioServer {
     public static void main(String[] args) throws IOException {
-        String song = "/Users/sampastoriza/Documents/Programming/Java Development/AudioStreaming/song.mp3";
-        File soundFile = AudioUtil.getSoundFile(song);
-
-        System.out.println("server: " + soundFile);
-
-        try (ServerSocket serverSocker = new ServerSocket(6666);
-             FileInputStream in = new FileInputStream(soundFile)) {
-            if (serverSocker.isBound()) {
-                Socket client = serverSocker.accept();
-                OutputStream out = client.getOutputStream();
-
-                byte buffer[] = new byte[2048];
-                int count;
-                while ((count = in.read(buffer)) != -1)
-                    out.write(buffer, 0, count);
+        try (ServerSocket serverSocket = new ServerSocket(6666)) {
+            while(true) {
+                final Socket client = serverSocket.accept();
+                new Thread(new AudioStream(client)).start();
             }
         }
-
-        System.out.println("server: shutdown");
     }
 }
---
+++ AudioStreaming/src/AudioStream.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Created by sampastoriza on 4/16/17.
+ */
+
+public class AudioStream implements Runnable {
+    private Socket client;
+    private String song = "/Users/sampastoriza/Documents/Programming/Python Development/pandora-clone/AudioStreaming/song.wav";
+    private File soundFile;
+
+    public AudioStream(Socket client) {
+        this.client = client;
+        this.soundFile = AudioUtil.getSoundFile(song);
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            OutputStream out = this.client.getOutputStream();
+            FileInputStream in = new FileInputStream(this.soundFile);
+            byte buffer[] = new byte[2048];
+            int count;
+            while ((count = in.read(buffer)) != -1)
+                out.write(buffer, 0, count);
+        } catch (IOException e) {
+            System.out.println();
+            e.printStackTrace();
+        }
+    }
+}
